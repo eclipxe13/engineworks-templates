55437e09dd5278810fcb6cca36f315c9c3ded808
Carlos C Soto <eclipxe13@gmail.com>
2016-09-23 20:31:50 -0500

    Add plugin to integrate with Slim and provide functions pathFor and baseUrl

---
+++ sources/EngineWorks/Templates/Slim/SlimPlugin.php
@@ -0,0 +1,50 @@
+<?php
+namespace EngineWorks\Slim;
+
+use Slim\Interfaces\RouterInterface;
+use EngineWorks\Templates\Plugin;
+
+class SlimPlugin implements Plugin
+{
+    public function getCallablesTable()
+    {
+        return [
+            'pathFor' => 'pathFor',
+            'baseUrl' => 'baseUrl',
+        ];
+    }
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * @var string
+     */
+    private $baseUrl;
+
+    /**
+     * SlimPlugin constructor.
+     * @param RouterInterface $router
+     * @param string $baseUrl
+     */
+    public function __construct(RouterInterface $router, $baseUrl)
+    {
+        if (! is_string($baseUrl)) {
+            throw new \InvalidArgumentException("baseUrl must be a string or an Uri instance");
+        }
+        $this->router = $router;
+        $this->baseUrl = $baseUrl;
+    }
+
+    public function pathFor($name, array $data = [], array $queryParams = [])
+    {
+        return $this->router->pathFor($name, $data, $queryParams);
+    }
+
+    public function baseUrl()
+    {
+        return $this->baseUrl;
+    }
+}
